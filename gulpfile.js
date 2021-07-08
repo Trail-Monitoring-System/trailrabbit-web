fee3466e97eaf11fff9792546b8b196cea891ef8
Brendan Drew <brendan.drew@autodesk.com>
2021-07-07 22:27:13 -0400

    fix amplify gulpfile build error

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
+const licenses = () => {
 gulp.task("licenses", async function () {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
@@ -81,3 +82,6 @@ gulp.task("licenses", async function () {
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
 });
+}
+
+exports.default = licenses();
